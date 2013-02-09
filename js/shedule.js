9a5f1cef0292e63b8a1b91fd76723923b8308b68
vindm <vinokuroff.dm@gmail.com>
2013-02-09 08:53:35 +0400

    fix

--- js/shedule.js
+++ js/shedule.js
@@ -1502,8 +1502,9 @@ $(function() {
         }
         auth_key = flashVars['auth_key'];
         viewer_id = flashVars['viewer_id'];
-        console.log(VK)
+        console.log(parts)
         VK.api("getGroups", function(data) {
+            console.log('lol')
             for(var i=0; i<=data.response.length;i++) {
                 switch(data.response[i]) {
                     case 482775:
